ab5eb9fc4d288c5b7daac44dbc8569c9189b8345
Charles <charlesguertin@live.ca>
2018-05-13 23:24:41 -0400

    Cluster testing

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const express = require('express');
-const app = express();
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require('cluster');
 
-function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {}
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+    // Cause index.js to be executed *again* but
+    // in child mode
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+} else {
+    // I'm a child, i'm going to act like a server
+    // and nothing else
+    const express = require('express');
+    const crypto = require('crypto');
+    const app = express();
+    
+    app.get('/', (req, res) => {
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send('Hi there');
+        });
+    });
+
+    app.get('/fast', (req, res) => {
+        res.send('This was fast!!');
+    });
+    
+    app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
-});
-
-app.listen(3000);
